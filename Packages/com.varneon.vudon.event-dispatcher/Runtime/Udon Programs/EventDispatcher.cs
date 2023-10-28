32572c96f366c7429755b941a62a19f0cfd453f4
Varneon <me@varneon.com>
2023-10-28 09:55:37 -0700

    Exclude dispatcher from preset

--- Packages/com.varneon.vudon.event-dispatcher/Runtime/Udon Programs/EventDispatcher.cs
+++ Packages/com.varneon.vudon.event-dispatcher/Runtime/Udon Programs/EventDispatcher.cs
@@ -14,6 +14,7 @@ namespace Varneon.VUdon.EventDispatcher
     /// <remarks>
     /// Default execution order of the dispatcher is 0
     /// </remarks>
+    [ExcludeFromPreset]
     [DisallowMultipleComponent]
     [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
     public class EventDispatcher : UdonSharpBehaviour
